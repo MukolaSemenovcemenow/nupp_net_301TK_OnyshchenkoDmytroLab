cab9c0a463f75d38e01d5b19b673820352760e24
Vlad <rgrkf148@gmail.com>
2025-05-31 23:15:38 +0300

    Добавьте файлы проекта.

---
+++ Lab2_0110D/Common/Charger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab2_0110D.Common
+{
+    public class Charger : Device
+    {
+        public int Power { get; set; }
+
+        public Charger(string name, double price, int power) : base(name, price)
+        {
+            Power = power;
+        }
+
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Потужність: {Power} Вт");
+        }
+
+        public static Charger CreateNew()
+        {
+            var rnd = new Random();
+            int[] powerOptions = { 18, 30, 45, 65, 100 };
+            return new Charger("Зарядка " + rnd.Next(1000), rnd.Next(400, 3000), powerOptions[rnd.Next(powerOptions.Length)]);
+        }
+    }
+}
